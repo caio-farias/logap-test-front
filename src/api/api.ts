f4797539ed0348641f2dc67131024143041fd956
Caio Farias <caio4095@gmail.com>
2022-07-12 19:38:34 -0300

    fix axios intrerceptor

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const baseURL = process.env.REACT_APP_API_URL
-const api = axios.create({ baseURL }).interceptors.request.use((config) => {
+const api = axios.create({ baseURL })
+
+api.interceptors.request.use((config) => {
 	const headers = { ...config.headers }
 
 	headers['Access-Control-Allow-Origin'] = '*'
