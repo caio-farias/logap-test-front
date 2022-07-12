d4542fe2fe9f46f9d2e7a8a0474eec40357856ce
Caio Farias <caio4095@gmail.com>
2022-07-12 19:35:23 -0300

    fix axios headers

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,13 @@
 import axios from 'axios'
 
 const baseURL = process.env.REACT_APP_API_URL
-const api = axios.create({ baseURL })
+const api = axios.create({ baseURL }).interceptors.request.use((config) => {
+	const headers = { ...config.headers }
+
+	headers['Access-Control-Allow-Origin'] = '*'
+	headers['Content-Type'] = 'application/json'
+
+	return { ...config, headers }
+})
 
 export default api
